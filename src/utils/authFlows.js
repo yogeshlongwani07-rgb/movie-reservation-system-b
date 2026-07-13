ac30d891b80a64bd3b363853f1228aaf9f2fe21a
yogeshlongwani07 <yogeshlongwani07@gmail.com>
2026-07-14 00:13:54 +0530

    Refactor authentication flows by adding refreshAccessToken and logout functions; update AdminDomain and UserDomain to utilize these new utilities

---
+++ src/utils/authFlows.js
@@ -0,0 +1,31 @@
+const AppError = require("./appError");
+const { generateAccessToken } = require("../utils/generateToken");
+const jwt = require("jsonwebtoken");
+
+async function refreshAccessToken(refreshToken, repository) {
+  if (!refreshToken) {
+    throw new AppError("Refresh token Not Found", 400);
+  }
+  const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+  const account = await repository.findById(decoded._id);
+  if (!account) {
+    throw new AppError("User not Found", 400);
+  }
+  if (account.refreshToken !== refreshToken) {
+    throw new AppError("Invalid refresh token", 405);
+  }
+  const accessToken = generateAccessToken(account);
+  return accessToken;
+}
+
+async function logout(id, repository) {
+  const account = await repository.findById(id);
+  if (!account) {
+    throw new AppError("User not found", 405);
+  }
+
+  account.refreshToken = null;
+  await repository.save(account);
+}
+
+module.exports = { logout, refreshAccessToken };
