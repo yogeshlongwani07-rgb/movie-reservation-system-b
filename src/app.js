41754f094198014b46b7351e0bca3b9032d9cfd0
yogeshlongwani07 <yogeshlongwani07@gmail.com>
2026-08-01 20:54:24 +0530

    refactor: remove Google OAuth2 implementation and related dependencies

--- src/app.js
+++ src/app.js
@@ -2,12 +2,10 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
 const cors = require("cors");
-const passport = require("./config/passport");
 const { limiter } = require("../src/middleware/rateLimiter");
 const adminRoutes = require("./routes/admin");
 const movieListingRoutes = require("./routes/movie");
 const userRoutes = require("./routes/user");
-const authRoutes = require("./routes/auth");
 const paymentRoutes = require("./routes/payment");
 const { corsOptions } = require("./Constants");
 const errorHandler = require("../src/middleware/errorHandler");
@@ -25,12 +23,10 @@ function createApp() {
   app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
   app.use(express.json());
-  app.use(passport.initialize());
 
   app.use("/api/v1/admin", adminRoutes);
   app.use("/api/v1/movie", movieListingRoutes);
   app.use("/api/v1/user", userRoutes);
-  app.use("/api/v1/auth", authRoutes);
   app.use("/api/v1/payment", paymentRoutes);
   app.use("/api/v1/o/auth", Oauth2Routes);
 
